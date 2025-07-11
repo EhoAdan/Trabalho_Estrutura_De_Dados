8ccd5e2ff1ac5d1b00da0747bb6c7ad63fe8a6d9
Marcos André Sant'Ana Ribeiro <macode2006@gmail.com>
2025-07-10 23:50:50 -0300

    Add files via upload

--- lista_invertida/main.py
+++ lista_invertida/main.py
@@ -1,17 +1,23 @@
 from diretorio import Diretorio
 from cadastro import Cadastro
 
-dic_total = [
-    {"id": "1", "nome" : "safado", "cidade": "Lages", "profissao": "Médico", "salario": 5000},
-    {"id": "2", "nome": "Bruno", "cidade": "Criciúma", "profissao": "Analista", "salario": 2200},
-    {"id": "3", "nome": "Carla", "cidade": "Florianópolis", "profissao": "Engenheiro", "salario": 4500}
-]
+cadastro = Cadastro()
 
-dic_profissão = Diretorio("profissao", tipo= "discreto")
-dic_salario = Diretorio("salario", tipo= "continuo")
-dic_cidade = Diretorio("cidade", tipo= "discreto")
+dir_profissao = Diretorio("profissao", tipo= "discreto")
+dir_salario = Diretorio("salario", tipo= "continuo")
+dir_cidade = Diretorio("cidade", tipo= "discreto")
 
+def carregar_dados():
 
+    dic_total = [
+        {"id": "1", "nome" : "safado", "cidade": "Lages", "profissao": "Médico", "salario": 5000},
+        {"id": "2", "nome": "Bruno", "cidade": "Criciúma", "profissao": "Analista", "salario": 2200},
+        {"id": "3", "nome": "Carla", "cidade": "Florianópolis", "profissao": "Engenheiro", "salario": 4500}
+    ]
 
+    for i in dic_total:
+        cadastro.inserir(i)
+        dir_cidade.adicionar(i)
+        dir_salario.adicionar(i)
+        dir_profissao.adicionar(i)
 
-print("Bem-Vindo! O que gostaria de fazer?")
