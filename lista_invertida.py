62b938cd1b0ff68c145312900b9020ca5fdcf1bc
EhoAdan <adansamuelpruss@gmail.com>
2025-07-10 22:57:37 -0300

    Carga feita

---
+++ lista_invertida.py
@@ -0,0 +1,20 @@
+dic_total = {
+    1: ["Maria", "Florianópolis", "Analista", 2000],
+    2: ["Luíza", "Florianópolis", "Gerente", 3000],
+    3: ["Fernanda", "Florianópolis", "Médico", 3500],
+    4: ["João", "Inbituba", "Analista", 4000],
+    5: ["José", "Inbituba", "Gerente", 4500],
+    6: ["Ayrton", "Inbituba", "Médico", 5000],
+    7: ["Camilo", "Jaraguá do Sul", "Analista", 9000],
+    8: ["Felipe", "Jaraguá do Sul", "Gerente", 12000],
+    9: ["Gustavo", "Jaraguá do Sul", "Médico", 15000]
+    }
+
+dic_profissão = {}
+dic_salário = {}
+dic_cidade = {}
+
+print("Bem-Vindo! O que gostaria de fazer?")
+
+print(len(dic_total))
+print(dic_total.values())
