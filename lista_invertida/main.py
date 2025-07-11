16daceeb96c9841a5f1485ae362537239776dbee
EhoAdan <adansamuelpruss@gmail.com>
2025-07-11 00:07:36 -0300

    Identação corrigida

--- lista_invertida/main.py
+++ lista_invertida/main.py
@@ -9,15 +9,14 @@ dir_cidade = Diretorio("cidade", tipo= "discreto")
 
 def carregar_dados():
 
-dic_total = [
-    {"id": "1", "nome" : "George", "cidade": "Balneário Camboriú", "cargo": "Gerente", "salario": 5000},
-    {"id": "2", "nome": "Bruno", "cidade": "São José", "cargo": "Analista", "salario": 2200},
-    {"id": "3", "nome": "Carla", "cidade": "Florianópolis", "cargo": "Engenheiro", "salario": 4500}
-]
+    dic_total = [
+        {"id": "1", "nome" : "George", "cidade": "Balneário Camboriú", "cargo": "Gerente", "salario": 5000},
+        {"id": "2", "nome": "Bruno", "cidade": "São José", "cargo": "Analista", "salario": 2200},
+        {"id": "3", "nome": "Carla", "cidade": "Florianópolis", "cargo": "Engenheiro", "salario": 4500}
+    ]
 
     for i in dic_total:
         cadastro.inserir(i)
         dir_cidade.adicionar(i)
         dir_salario.adicionar(i)
         dir_profissao.adicionar(i)
-
