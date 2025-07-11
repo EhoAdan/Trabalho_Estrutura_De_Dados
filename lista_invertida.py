b53458be7a05730bf674ef681521fa2e3915070c
EhoAdan <adansamuelpruss@gmail.com>
2025-07-10 23:12:57 -0300

    Update

--- lista_invertida.py
+++ lista_invertida.py
@@ -1,20 +1,29 @@
 dic_total = {
     1: ["Maria", "Florianópolis", "Analista", 2000],
     2: ["Luíza", "Florianópolis", "Gerente", 3000],
-    3: ["Fernanda", "Florianópolis", "Médico", 3500],
+    3: ["José", "Florianópolis", "Médico", 3500],
     4: ["João", "Inbituba", "Analista", 4000],
     5: ["José", "Inbituba", "Gerente", 4500],
     6: ["Ayrton", "Inbituba", "Médico", 5000],
     7: ["Camilo", "Jaraguá do Sul", "Analista", 9000],
-    8: ["Felipe", "Jaraguá do Sul", "Gerente", 12000],
+    8: ["Maria", "Jaraguá do Sul", "Gerente", 12000],
     9: ["Gustavo", "Jaraguá do Sul", "Médico", 15000]
     }
 
-dic_profissão = {}
-dic_salário = {}
+dic_profissao = {}
+dic_salario = {}
 dic_cidade = {}
 
-print("Bem-Vindo! O que gostaria de fazer?")
+print(dic_total[1])
 
-print(len(dic_total))
-print(dic_total.values())
+def inserir(nome, cidade, profissao, salario):
+    dic_total.update({nome: [cidade, profissao, salario]})
+
+def busca_cidade(cidade):
+    print(dic_profissao[cidade])
+
+def busca_profissão(profissao):
+    print(dic_profissao[profissao])
+
+def busca_salario(salario):
+    print(dic_profissao[salario])
